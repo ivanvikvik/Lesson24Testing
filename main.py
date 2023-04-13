a5dbba6ae1523d0b7d1b2b780e8c379a14787972
ivanvikvik <ivanvikvik@gmail.com>
2023-04-13 21:52:26 +0300

    Change some code

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+class Student:
+    def __init__(self, name='no name'):
+        self.name = name
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # def __str__(self):
+    #     return self.name
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+st = Student("Alex")
+s = "Student: " + str(st)
+print(s)
